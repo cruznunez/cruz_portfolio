e69e490257edab12cc1bc17be7c99bfd2518781e
Cruz Nunez <nunez.a.cruz@gmail.com>
2015-10-23 14:57:51 -0400

    Refactor css and update resume

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,21 +7,25 @@ function lockNav() {
   var windowHeight = $(window).height();
   var contactInfoHeight = $('div.container-black').outerHeight( true );
   var blackHeight = windowHeight + headerHeight - contactInfoHeight
-  if($(window).scrollTop() > headerHeight && $(window).width() > 672){
-    if($('#nav').css('position') == 'absolute'){
-      $('#nav').css({'position' : 'fixed', 'margin-top' : '0', 'top' : '5vh'});
-      $('div.container-black').css({'position' : 'fixed', 'bottom' : '0', 'top' : ''});
+  if ($(window).width() > 672) {
+    if($(window).scrollTop() > headerHeight ){
+      if($('#nav').css('position') == 'absolute'){
+        $('#nav').css({'position' : 'fixed', 'margin-top' : '0', 'top' : '5vh'});
+        $('div.container-black').css({'position' : 'fixed', 'bottom' : '0', 'top' : ''});
+      };
+    }else{
+      if($('#nav').css('position') == 'fixed'){
+        $('#nav').css({ 'position' : 'absolute', 'top' : '', 'margin-top' : ''});
+        $('div.container-black').css({'position' : 'absolute', 'bottom': '', 'top' : blackHeight });
+      };
     };
   }else{
-    if($('#nav').css('position') == 'fixed'){
-      $('#nav').css({ 'position' : 'absolute', 'top' : '', 'margin-top' : ''});
-      $('div.container-black').css({'position' : 'absolute', 'bottom': '', 'top' : blackHeight });
-    };
-  };
+    $('#nav').css({ 'position' : '', 'top' : '', 'margin-top' : ''});
+  }
 };
 
 function masterLock() {
-  $( window ).scroll(lockNav);
+  $( window ).on("resize scroll",lockNav);
 };
 
 function makeAllLinksOpenInNewWindowExceptForNavigation() {
@@ -39,21 +43,6 @@ function placeInitialBlackContactInfo() {
   $('div.container-black').css('top', blackHeight );
 }
 
-function resetColor() {
-  $(window).on("load", function() {
-    css = $('style').last().html();
-  });
-
-  $(window).on("load resize", function(e){
-    if ($(window).width() > 672) {
-      $('style').last().html("");
-    } else {
-      $('style').last().html( css );
-    };
-  });
-};
-
 $(placeInitialBlackContactInfo);
 $(masterLock);
 $(makeAllLinksOpenInNewWindowExceptForNavigation);
-$(resetColor);
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def navigation(page)
+    href = {"Home" => "/", "Projects" => "projects", "Resumé" => "resume" }[page]
+    css_class = @page == page ? "button selected" : "button"
+    icon_class = {"Home" => "fa fa-home", "Projects" => "fa fa-file-code-o", "Resumé" => "fa fa-check-square-o" }[page]
+    "<a href='#{href}'><span class='#{css_class}'><i class='#{icon_class}'></i>#{page}</span></a>".html_safe
+  end
 end
